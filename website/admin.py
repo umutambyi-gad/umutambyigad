4a06f3a242ed0bb8023e77c7f0b02bd430d1a964
Umutambyi Gad <umutambyig@gmail.com>
2021-02-15 19:45:18 +0100

    Registered model in the admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+	Careers,
+	Introduction
+)
+
 
 # Register your models here.
+class CarrersAdmin(admin.ModelAdmin):
+	list_display = ('career', 'added_date')
+
+admin.site.register(Carrers, CarrersAdmin)
+
+
+class IntroductionAdmin(admin.ModelAdmin):
+	def save_model(self, request, obj, form, change):
+		obj.username = request.user
+		return super().save_model(request, obj, form, change)
+
+	list_display = ('username', 'added_date')
+
+admin.site.register(Introduction, IntroductionAdmin)
--- website/models.py
+++ website/models.py
@@ -4,12 +4,29 @@ from django.db import models
 # Create your models here.
 class Careers(models.Model):
 	career = models.CharField(max_length=250)
+	added_date = models.DateTimeField(auto_now_add=True)
+	
+
+	def __str__(self):
+		return self.career
 
 
 class Introduction(models.Model):
+	username = models.CharField(max_length=200)
 	background_image = models.ImageField(upload_to='images')
 	salutation = models.CharField(max_length=250)
-	names = models.CharField(max_length=300)
+	about = models.CharField(max_length=300)
 	careers = models.ManyToManyField(Careers)
 	added_date = models.DateTimeField(auto_now_add=True)
+
+
+	# This method will avoid to allow more than one instance being saved
+	def save(self, *args, **kwargs):
+		if not self.pk and Introduction.objects.exists():
+			return False
+		super().save(*args, **kwargs)
+
+
+	def __str__(self):
+		return self.username
 
