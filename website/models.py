29199d5949fe10444f0cfebc09421182a8ca95e1
Umutambyi Gad <umutambyig@gmail.com>
2021-02-15 19:28:25 +0100

    Added new field to track instance's added date

--- website/models.py
+++ website/models.py
@@ -5,8 +5,11 @@ from django.db import models
 class Careers(models.Model):
 	career = models.CharField(max_length=250)
 
+
 class Introduction(models.Model):
 	background_image = models.ImageField(upload_to='images')
 	salutation = models.CharField(max_length=250)
 	names = models.CharField(max_length=300)
 	careers = models.ManyToManyField(Careers)
+	added_date = models.DateTimeField(auto_now_add=True)
+
