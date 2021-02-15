b61b7c6231672b596082376f7bbce89a37975e96
Umutambyi Gad <umutambyig@gmail.com>
2021-02-15 19:26:14 +0100

    Wrote first model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Careers(models.Model):
+	career = models.CharField(max_length=250)
+
+class Introduction(models.Model):
+	background_image = models.ImageField(upload_to='images')
+	salutation = models.CharField(max_length=250)
+	names = models.CharField(max_length=300)
+	careers = models.ManyToManyField(Careers)
