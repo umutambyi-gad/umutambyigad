75397d239aece1e105613b1edaa3f179112c7688
Umutambyi Gad <umutambyig@gmail.com>
2021-02-16 17:33:58 +0100

    Removed save method where is was not needed

--- website/models.py
+++ website/models.py
@@ -65,13 +65,6 @@ class Skills(models.Model):
 	added_date = models.DateTimeField(auto_now_add=True)
 
 
-	# This method will avoid to allow more than one instance being saved
-	def save(self, *args, **kwargs):
-		if not self.pk and Introduction.objects.exists():
-			return False
-		super().save(*args, **kwargs)
-
-
 	def __str__(self):
 		return self.name
 
