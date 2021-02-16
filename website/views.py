3dc07e79d862d2e44cbd06e80493387e79fb3c5f
Umutambyi Gad <umutambyig@gmail.com>
2021-02-16 23:03:49 +0100

    Made some queries in views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 from django.views import View
+from .models import (
+	Introduction,
+	Careers,
+	Profile,
+	Skills,
+	ServiceSection,
+	Services,
+	StatisticSection,
+	Statistics,
+	PortifolioSection,
+	Portifolio,
+	Categories,
+	BlogSection,
+	Blogs,
+	Contact,
+	SocialMedia
+)
 
 
 # Create your views here.
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
-		return render(request, 'index.html')
+		context = {
+		'Intro': Introduction.objects.first(),
+		'careers': Careers.objects.all(),
+		'profile': Profile.objects.first(),
+		'skills': Skills.objects.all(),
+		'service_section': ServiceSection.objects.first(),
+		'services': Services.objects.all(),
+		'stats_sections': StatisticSection.objects.first(),
+		'stats': Statistics.objects.all(),
+		'portifolio_section': PortifolioSection.objects.first(),
+		'portifolios': Portifolio.objects.all()
+		}
+		return render(request, 'index.html', context)
+
+
+	def post(self, request, *args, **kwargs):
+		pass
