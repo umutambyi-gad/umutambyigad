feb9763644035b5c5aaedc37b8d20550328b0ccf
Umutambyi Gad <umutambyig@gmail.com>
2021-02-18 18:22:37 +0100

    Added admin urls to environment variable

--- umutambyigad/urls.py
+++ umutambyigad/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from decouple import config
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(config('admin_url'), admin.site.urls),
     path('', include('website.urls'))
 ]
 
